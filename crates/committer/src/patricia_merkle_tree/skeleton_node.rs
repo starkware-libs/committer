e24e9dc9d1a37df698f6d6d136599ceaf112379f
amos rothberg <amos@starkware.co>
2024-03-24 19:15:34 +0200

    build: add tentative tree API

---
+++ crates/committer/src/patricia_merkle_tree/skeleton_node.rs
@@ -0,0 +1,10 @@
+use crate::hash::types::HashOutput;
+use crate::patricia_merkle_tree::types::{LeafTrait, PathToBottom};
+
+#[allow(dead_code)]
+pub(crate) enum SkeletonNode<L: LeafTrait> {
+    Binary,
+    Edge { path_to_bottom: PathToBottom },
+    Sibling(HashOutput),
+    Leaf(L),
+}

217642448d0ae8a18200c7a08352bd74e023d1eb
amos rothberg <amos@starkware.co>
2024-03-24 19:15:34 +0200

    build: add tentative tree API

---
+++ crates/committer/src/patricia_merkle_tree/tree.rs
@@ -0,0 +1,12 @@
+use crate::patricia_merkle_tree::node::Node;
+
+trait Tree {
+    /// Returns the node with given full (Merkle) index, if it exists.
+    fn get_node(full_index: u128) -> Option<Node>;
+
+    /// Returns the root if the tree is not empty.
+    fn get_root() -> Option<Node>;
+
+    /// Computes the hash of the given node.
+    fn compute_hash(node: Node) -> [u8; 32];
+}

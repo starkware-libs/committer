08468176c54fb1c78496db0e8fed0c94c6c7d92d
AvivYossef-starkware <aviv.y@starkware.co>
2024-04-07 21:35:22 +0300

    add node_prefix func

--- crates/committer/src/types.rs
+++ crates/committer/src/types.rs
@@ -1,3 +1,15 @@
 use starknet_types_core::felt::Felt as StarknetTypesFelt;
 
 pub(crate) type Felt = StarknetTypesFelt;
+
+pub(crate) trait FeltTrait {
+    fn to_bytes(&self) -> [u8; 32];
+}
+
+
+impl FeltTrait for Felt {
+    fn to_bytes(&self) -> [u8; 32] {
+        self.to_bytes_be()
+    }
+
+}

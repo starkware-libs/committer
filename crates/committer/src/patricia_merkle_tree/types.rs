51302d1cd6d4d2427fb046b91d96758430c72705
Aner Ben Efraim <aner@starkware.co>
2024-04-08 20:30:16 +0300

    build: add root_index and compute_bottom_index

--- crates/committer/src/patricia_merkle_tree/types.rs
+++ crates/committer/src/patricia_merkle_tree/types.rs
@@ -2,14 +2,33 @@ use crate::hash::types::{HashFunction, HashOutput};
 use crate::patricia_merkle_tree::filled_node::NodeData;
 use crate::types::Felt;
 
+#[cfg(test)]
+#[path = "types_test.rs"]
+pub mod types_test;
+
 pub(crate) trait TreeHashFunction<L: LeafDataTrait, H: HashFunction> {
     /// Computes the hash of given node data.
     fn compute_node_hash(node_data: NodeData<L>) -> HashOutput;
 }
 
-// TODO(Amos, 01/05/2024): Implement types for NodeIndex, EdgePath, EdgePathLength
 #[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub(crate) struct NodeIndex(pub Felt);
+
+#[allow(dead_code)]
+impl NodeIndex {
+    pub(crate) fn root_index() -> NodeIndex {
+        NodeIndex(Felt::ONE)
+    }
+
+    pub(crate) fn compute_bottom_index(
+        index: NodeIndex,
+        path_to_bottom: PathToBottom,
+    ) -> NodeIndex {
+        let PathToBottom { path, length } = path_to_bottom;
+        NodeIndex(index.0 * Felt::TWO.pow(length.0) + path.0)
+    }
+}
 
 #[allow(dead_code)]
 pub(crate) struct EdgePath(pub Felt);

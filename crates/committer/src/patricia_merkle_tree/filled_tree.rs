89bf927edbdf03fc3b057b7a98c180c388b57731
Aner Ben Efraim <aner@starkware.co>
2024-04-09 09:34:40 +0300

    fix: change FilledTree signature for interior mutability

--- crates/committer/src/patricia_merkle_tree/filled_tree.rs
+++ crates/committer/src/patricia_merkle_tree/filled_tree.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex, RwLock};
 
 use crate::patricia_merkle_tree::filled_node::FilledNode;
 use crate::patricia_merkle_tree::types::{LeafDataTrait, NodeIndex};
@@ -7,5 +8,22 @@ use crate::patricia_merkle_tree::types::{LeafDataTrait, NodeIndex};
 /// FilledTree consists of all nodes which were modified in the update, including their updated
 /// data and hashes.
 pub(crate) trait FilledTree<L: LeafDataTrait> {
-    fn get_all_nodes(&self) -> HashMap<NodeIndex, &FilledNode<L>>;
+    fn get_all_nodes(&self) -> Arc<RwLock<HashMap<NodeIndex, Mutex<FilledNode<L>>>>>;
+}
+
+pub(crate) struct FilledTreeImpl<L: LeafDataTrait> {
+    tree_map: Arc<RwLock<HashMap<NodeIndex, Mutex<FilledNode<L>>>>>,
+}
+
+#[allow(dead_code)]
+impl<L: LeafDataTrait> FilledTreeImpl<L> {
+    pub(crate) fn new(tree_map: Arc<RwLock<HashMap<NodeIndex, Mutex<FilledNode<L>>>>>) -> Self {
+        Self { tree_map }
+    }
+}
+
+impl<L: LeafDataTrait> FilledTree<L> for FilledTreeImpl<L> {
+    fn get_all_nodes(&self) -> Arc<RwLock<HashMap<NodeIndex, Mutex<FilledNode<L>>>>> {
+        Arc::clone(&self.tree_map)
+    }
 }

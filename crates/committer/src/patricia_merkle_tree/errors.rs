e1256b02b5e7ad195a583e36ec3109c8752c4422
Nimrod Weiss <nimrod@starkware.co>
2024-04-11 09:00:30 +0300

    chore: building original skeleton

--- crates/committer/src/patricia_merkle_tree/errors.rs
+++ crates/committer/src/patricia_merkle_tree/errors.rs
@@ -1,6 +1,25 @@
+use thiserror::Error;
+
+use crate::patricia_merkle_tree::types::NodeIndex;
+use crate::storage::errors::StorageError;
+use crate::storage::storage_trait::StorageValue;
+
 // TODO(Amos, 01/04/2024): Add error types.
-#[derive(Debug)]
-pub(crate) enum OriginalSkeletonTreeError {}
+#[allow(dead_code)]
+#[derive(Debug, Error)]
+pub(crate) enum OriginalSkeletonTreeError {
+    #[error(
+        "Failed to deserialize the storage value: {0:?} while building the original skeleton tree."
+    )]
+    Deserialization(StorageValue),
+    #[error(
+        "Unable to read from storage the storage key: {0:?} while building the \
+         original skeleton tree."
+    )]
+    StorageRead(#[from] StorageError),
+    #[error("`fetch_nodes` method encountered unexpectedally a leaf node at index: {0:?}")]
+    LeafEncountered(NodeIndex),
+}
 
 #[derive(Debug)]
 #[allow(dead_code)]

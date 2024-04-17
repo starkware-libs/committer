20edd4064af03041c8ae8a62c4d79861befcfa1a
AvivYossef-starkware <aviv.y@starkware.co>
2024-04-17 10:59:27 +0300

    build: add serde_trait.rs

--- crates/committer/src/storage/errors.rs
+++ crates/committer/src/storage/errors.rs
@@ -1,2 +1,11 @@
+use derive_more::Display;
+
 #[derive(Debug)]
 pub(crate) enum StorageError {}
+
+#[derive(thiserror::Error, Debug, Display)]
+#[allow(dead_code)]
+pub(crate) enum SerdeError {
+    DeserializeError,
+    SerializeError,
+}

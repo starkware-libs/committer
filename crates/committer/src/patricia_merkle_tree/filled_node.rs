c70f694efcd25972b86c5cf86cc5970f27406918
Aner Ben Efraim <aner@starkware.co>
2024-04-17 15:00:35 +0300

    refactor: remove rwlock

--- crates/committer/src/patricia_merkle_tree/filled_node.rs
+++ crates/committer/src/patricia_merkle_tree/filled_node.rs
@@ -8,7 +8,6 @@ pub(crate) struct ClassHash(pub Felt);
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub(crate) struct Nonce(pub Felt);
 
-#[allow(dead_code)]
 #[derive(Clone, Debug, PartialEq, Eq)]
 /// A node in a Patricia-Merkle tree which was modified during an update.
 pub(crate) struct FilledNode<L: LeafDataTrait> {
@@ -16,7 +15,6 @@ pub(crate) struct FilledNode<L: LeafDataTrait> {
     pub(crate) data: NodeData<L>,
 }
 
-#[allow(dead_code)]
 #[derive(Clone, Debug, PartialEq, Eq)]
 // A Patricia-Merkle tree node's data, i.e., the pre-image of its hash.
 pub(crate) enum NodeData<L: LeafDataTrait> {
@@ -25,7 +23,7 @@ pub(crate) enum NodeData<L: LeafDataTrait> {
     Leaf(L),
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub(crate) struct BinaryData {
     pub(crate) left_hash: HashOutput,
     pub(crate) right_hash: HashOutput,

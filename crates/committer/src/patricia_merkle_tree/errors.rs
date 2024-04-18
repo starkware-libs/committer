fa95773edb10a80bb5c07cef2f99d7458cfa19e0
Aner Ben Efraim <aner@starkware.co>
2024-04-18 11:49:21 +0300

    fix: add details to updated_skeleton_errrors

--- crates/committer/src/patricia_merkle_tree/errors.rs
+++ crates/committer/src/patricia_merkle_tree/errors.rs
@@ -1,13 +1,22 @@
 // TODO(Amos, 01/04/2024): Add error types.
 
+use crate::patricia_merkle_tree::types::NodeIndex;
+
+use crate::patricia_merkle_tree::filled_node::FilledNode;
+
+use super::types::LeafDataTrait;
+
 #[derive(Debug)]
 pub(crate) enum OriginalSkeletonTreeError {}
 
 #[derive(Debug)]
 #[allow(dead_code)]
-pub(crate) enum UpdatedSkeletonTreeError {
-    MissingNode,
-    DoubleUpdate,
+pub(crate) enum UpdatedSkeletonTreeError<L: LeafDataTrait> {
+    MissingNode(NodeIndex),
+    DoubleUpdate {
+        index: NodeIndex,
+        existing_value: Box<FilledNode<L>>,
+    },
     PoisonedLock(String),
     NonDroppedPointer(String),
 }

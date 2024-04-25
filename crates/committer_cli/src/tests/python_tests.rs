ec60d436666a564daeec26188fbde7ec8f9cf486
Nimrod Weiss <nimrod@starkware.co>
2024-04-25 11:02:56 +0300

    chore: add serde test

--- crates/committer_cli/src/tests/python_tests.rs
+++ crates/committer_cli/src/tests/python_tests.rs
@@ -1,9 +1,13 @@
-use std::collections::HashMap;
+use committer::storage::errors::DeserializationError;
+use std::{collections::HashMap, fs};
 use thiserror;
+
+use crate::deserialization::{read::parse_input, types::Input};
 
 // Enum representing different Python tests.
 pub(crate) enum PythonTest {
     ExampleTest,
+    InputParsing,
 }
 
 /// Error type for PythonTest enum.
@@ -13,6 +17,8 @@ pub(crate) enum PythonTestError {
     UnknownTestName(String),
     #[error("Failed to parse input: {0}")]
     ParseInputError(#[from] serde_json::Error),
+    #[error("Test failed. {0}")]
+    DeserializationTestFailure(#[from] DeserializationError),
 }
 
 /// Implements conversion from a string to a `PythonTest`.
@@ -22,6 +28,7 @@ impl TryFrom<String> for PythonTest {
     fn try_from(value: String) -> Result<Self, Self::Error> {
         match value.as_str() {
             "example_test" => Ok(Self::ExampleTest),
+            "input_parsing" => Ok(Self::InputParsing),
             _ => Err(PythonTestError::UnknownTestName(value)),
         }
     }
@@ -35,6 +42,7 @@ impl PythonTest {
                 let example_input: HashMap<String, String> = serde_json::from_str(input)?;
                 Ok(example_test(example_input))
             }
+            Self::InputParsing => parse_input_test(input),
         }
     }
 }
@@ -44,4 +52,34 @@ pub(crate) fn example_test(test_args: HashMap<String, String>) -> String {
     let y = test_args.get("y").expect("Failed to get value for key 'y'");
 
     format!("Calling example test with args: x: {}, y: {}", x, y)
+}
+
+pub(crate) fn parse_input_test(temp_input_path: &str) -> Result<String, PythonTestError> {
+    let input = fs::read_to_string(temp_input_path).expect("Unable to read given file.");
+    Ok(create_output_to_python(parse_input(input)?))
+}
+
+fn create_output_to_python(actual_input: Input) -> String {
+    format!(
+        r#"
+        {{
+        "storage_size": {},
+        "address_to_class_hash_size": {},
+        "address_to_nonce_size": {},
+        "class_hash_to_compiled_class_hash": {},
+        "current_contract_state_leaves_size": {},
+        "outer_storage_updates_size": {},
+        "tree_height": {}
+        }}"#,
+        actual_input.storage.len(),
+        actual_input.state_diff.address_to_class_hash.len(),
+        actual_input.state_diff.address_to_nonce.len(),
+        actual_input
+            .state_diff
+            .class_hash_to_compiled_class_hash
+            .len(),
+        actual_input.state_diff.current_contract_state_leaves.len(),
+        actual_input.state_diff.storage_updates.len(),
+        actual_input.tree_height.0
+    )
 }

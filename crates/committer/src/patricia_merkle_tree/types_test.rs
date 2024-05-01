994371c76cc1070f25bdc325be7858b01d2ba3e1
Nimrod Weiss <nimrod@starkware.co>
2024-05-01 16:46:09 +0300

    build: create lower trees skeletons

--- crates/committer/src/patricia_merkle_tree/types_test.rs
+++ crates/committer/src/patricia_merkle_tree/types_test.rs
@@ -1,6 +1,8 @@
+use crate::block_committer::input::StarknetStorageKey;
 use crate::felt::Felt;
 use crate::patricia_merkle_tree::node_data::inner_node::{EdgePath, EdgePathLength, PathToBottom};
 use crate::patricia_merkle_tree::types::NodeIndex;
+use crate::patricia_merkle_tree::types::TreeHeight;
 use rstest::rstest;
 
 #[rstest]
@@ -23,4 +25,22 @@ fn test_compute_bottom_index(
     );
     let expected = NodeIndex::from(expected);
     assert_eq!(bottom_index, expected);
+}
+
+#[rstest]
+#[case(0, 127)]
+#[case(15, 118)]
+#[case(0xDEADBEEF, 60)]
+fn test_starknet_storage_key_to_node_index(#[case] node_index: u128, #[case] tree_height: u8) {
+    assert!(
+        node_index < 2_u128.pow(tree_height.into()),
+        "Invalid test arguments. The node index must be smaller than the number of nodes."
+    );
+    let expected = NodeIndex::from(2_u128.pow(tree_height.into()) | node_index);
+    let actual = NodeIndex::from_starknet_storage_key(
+        &StarknetStorageKey(Felt::from(node_index)),
+        &TreeHeight(tree_height),
+    );
+
+    assert_eq!(actual, expected);
 }

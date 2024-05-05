228255b2e6540afc0e54944d9cba6de6e3fe64f5
Nimrod Weiss <nimrod@starkware.co>
2024-05-05 12:30:01 +0300

    refactor: renamed parse_input/types => parse_input/raw_input

--- crates/committer_cli/src/parse_input/read.rs
+++ crates/committer_cli/src/parse_input/read.rs
@@ -1,6 +1,6 @@
 use committer::{block_committer::types::Input, storage::errors::DeserializationError};
 
-use crate::parse_input::types::RawInput;
+use crate::parse_input::raw_input::RawInput;
 
 #[cfg(test)]
 #[path = "read_test.rs"]

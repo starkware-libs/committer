dab06e23bd200c1f6512386da383004ebc0021da
Tzahi Taub <tzahi@starkware.co>
2024-05-22 10:47:55 +0300

    feat(skeleton): add_node_from_edge_data

--- crates/committer/src/patricia_merkle_tree/filled_tree/tree.rs
+++ crates/committer/src/patricia_merkle_tree/filled_tree/tree.rs
@@ -178,7 +178,7 @@ impl<L: LeafData> FilledTreeImpl<L> {
                 if skeleton_leaf.is_empty() != leaf_data.is_empty() {
                     return Err(FilledTreeError::<L>::InconsistentModification {
                         index,
-                        skeleton_leaf: skeleton_leaf.clone(),
+                        skeleton_leaf: *skeleton_leaf,
                     });
                 }
                 let node_data = NodeData::Leaf(leaf_data);

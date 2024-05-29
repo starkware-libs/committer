5a4a5e35b8098ae37ba450e5a9cf4553f1455af0
Dori Medini <dori@starkware.co>
2024-05-29 09:18:17 +0300

    feat: move leaf fetching logic to leaf data trait
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/committer/src/patricia_merkle_tree/node_data/errors.rs
+++ crates/committer/src/patricia_merkle_tree/node_data/errors.rs
@@ -2,6 +2,7 @@ use std::fmt::Debug;
 use thiserror::Error;
 
 use crate::patricia_merkle_tree::node_data::inner_node::EdgePathLength;
+use crate::patricia_merkle_tree::types::NodeIndex;
 
 #[derive(Debug, Error)]
 pub enum PathToBottomError {
@@ -11,3 +12,13 @@ pub enum PathToBottomError {
         n_edges: EdgePathLength,
     },
 }
+
+#[derive(Debug, Error)]
+pub enum LeafError {
+    #[error("Deleted leaf at index {0:?} appears in the updated skeleton tree.")]
+    DeletedLeafInSkeleton(NodeIndex),
+    #[error("Missing modification data at index {0:?}.")]
+    MissingDataForUpdate(NodeIndex),
+}
+
+pub type LeafResult<T> = Result<T, LeafError>;
--- crates/committer/src/patricia_merkle_tree/node_data/leaf.rs
+++ crates/committer/src/patricia_merkle_tree/node_data/leaf.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
+use std::future::Future;
 
 use crate::felt::Felt;
 use crate::hash::hash_trait::HashOutput;
 use crate::patricia_merkle_tree::filled_tree::node::{ClassHash, CompiledClassHash, Nonce};
+use crate::patricia_merkle_tree::node_data::errors::{LeafError, LeafResult};
 use crate::patricia_merkle_tree::types::NodeIndex;
 use crate::storage::db_object::DBObject;
 use strum_macros::{EnumDiscriminants, EnumIter};
@@ -10,7 +12,17 @@ use strum_macros::{EnumDiscriminants, EnumIter};
 pub trait LeafData: Clone + Sync + Send + DBObject {
     /// Returns true if leaf is empty.
     fn is_empty(&self) -> bool;
+
+    /// Creates a leaf.
+    // Use explicit desugaring of `async fn` to allow adding trait bounds to the return type, see
+    // https://blog.rust-lang.org/2023/12/21/async-fn-rpit-in-traits.html#async-fn-in-public-traits
+    // for details.
+    fn create(
+        index: &NodeIndex,
+        leaf_modifications: &LeafModifications<Self>,
+    ) -> impl Future<Output = LeafResult<Self>>;
 }
+
 #[allow(dead_code)]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct ContractState {
@@ -40,6 +52,20 @@ impl LeafData for LeafDataImpl {
                     && contract_state.storage_root_hash.0 == Felt::ZERO
             }
         }
+    }
+
+    async fn create(
+        index: &NodeIndex,
+        leaf_modifications: &LeafModifications<Self>,
+    ) -> LeafResult<Self> {
+        let leaf_data = leaf_modifications
+            .get(index)
+            .ok_or(LeafError::MissingDataForUpdate(*index))?
+            .clone();
+        if leaf_data.is_empty() {
+            return Err(LeafError::DeletedLeafInSkeleton(*index));
+        }
+        Ok(leaf_data)
     }
 }
 

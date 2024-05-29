35e9d916ea8a89b73492b01dcdd4aef87d38228e
Dori Medini <dori@starkware.co>
2024-05-29 09:18:17 +0300

    feat: move leaf fetching logic to leaf data trait
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/committer/src/patricia_merkle_tree/node_data/leaf.rs
+++ crates/committer/src/patricia_merkle_tree/node_data/leaf.rs
@@ -1,16 +1,31 @@
 use std::collections::HashMap;
+use std::future::Future;
 
 use crate::felt::Felt;
 use crate::hash::hash_trait::HashOutput;
+use crate::patricia_merkle_tree::filled_tree::errors::FilledTreeError;
 use crate::patricia_merkle_tree::filled_tree::node::{ClassHash, CompiledClassHash, Nonce};
+use crate::patricia_merkle_tree::filled_tree::tree::FilledTreeResult;
 use crate::patricia_merkle_tree::types::NodeIndex;
 use crate::storage::db_object::DBObject;
 use strum_macros::{EnumDiscriminants, EnumIter};
 
+use crate::patricia_merkle_tree::node_data::inner_node::NodeData;
+
 pub trait LeafData: Clone + Sync + Send + DBObject {
     /// Returns true if leaf is empty.
     fn is_empty(&self) -> bool;
+
+    /// Returns / computes the NodeData.
+    // Use explicit desugaring of `async fn` to allow adding trait bounds to the return type, see
+    // https://blog.rust-lang.org/2023/12/21/async-fn-rpit-in-traits.html#async-fn-in-public-traits
+    // for details.
+    fn get_node_data(
+        index: &NodeIndex,
+        leaf_modifications: &LeafModifications<Self>,
+    ) -> impl Future<Output = FilledTreeResult<NodeData<Self>, Self>>;
 }
+
 #[allow(dead_code)]
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct ContractState {
@@ -40,6 +55,24 @@ impl LeafData for LeafDataImpl {
                     && contract_state.storage_root_hash.0 == Felt::ZERO
             }
         }
+    }
+
+    async fn get_node_data(
+        index: &NodeIndex,
+        leaf_modifications: &LeafModifications<Self>,
+    ) -> FilledTreeResult<NodeData<Self>, Self> {
+        let leaf_data = leaf_modifications
+            .get(index)
+            .ok_or(FilledTreeError::<LeafDataImpl>::MissingDataForUpdate(
+                *index,
+            ))?
+            .clone();
+        if leaf_data.is_empty() {
+            return Err(FilledTreeError::<LeafDataImpl>::DeletedLeafInSkeleton(
+                *index,
+            ));
+        }
+        Ok(NodeData::Leaf(leaf_data))
     }
 }
 

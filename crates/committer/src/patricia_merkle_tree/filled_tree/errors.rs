28a4e664a3e307c282cbfe9d99ce514c23e7361a
Dori Medini <dori@starkware.co>
2024-05-29 09:18:17 +0300

    feat: move leaf fetching logic to leaf data trait
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/committer/src/patricia_merkle_tree/filled_tree/errors.rs
+++ crates/committer/src/patricia_merkle_tree/filled_tree/errors.rs
@@ -1,20 +1,19 @@
 use tokio::task::JoinError;
 
 use crate::patricia_merkle_tree::filled_tree::node::FilledNode;
+use crate::patricia_merkle_tree::node_data::errors::LeafError;
 use crate::patricia_merkle_tree::updated_skeleton_tree::errors::UpdatedSkeletonTreeError;
 use crate::patricia_merkle_tree::{node_data::leaf::LeafData, types::NodeIndex};
 
 #[derive(thiserror::Error, Debug)]
 pub enum FilledTreeError<L: LeafData> {
-    #[error("Deleted leaf at index {0:?} appears in the updated skeleton tree.")]
-    DeletedLeafInSkeleton(NodeIndex),
     #[error("Double update at node {index:?}. Existing value: {existing_value:?}.")]
     DoubleUpdate {
         index: NodeIndex,
         existing_value: Box<FilledNode<L>>,
     },
-    #[error("Missing modification data at index {0:?}.")]
-    MissingDataForUpdate(NodeIndex),
+    #[error(transparent)]
+    Leaf(#[from] LeafError),
     #[error("Missing node at index {0:?}.")]
     MissingNode(NodeIndex),
     #[error("Missing root.")]
--- crates/committer/src/patricia_merkle_tree/filled_tree/tree.rs
+++ crates/committer/src/patricia_merkle_tree/filled_tree/tree.rs
@@ -172,14 +172,8 @@ impl FilledTreeImpl {
             UpdatedSkeletonNode::Sibling(hash_result)
             | UpdatedSkeletonNode::UnmodifiedBottom(hash_result) => Ok(*hash_result),
             UpdatedSkeletonNode::Leaf => {
-                let leaf_data = leaf_modifications
-                    .get(&index)
-                    .ok_or(FilledTreeError::<LeafDataImpl>::MissingDataForUpdate(index))?
-                    .clone();
-                if leaf_data.is_empty() {
-                    return Err(FilledTreeError::DeletedLeafInSkeleton(index));
-                }
-                let node_data = NodeData::Leaf(leaf_data);
+                let node_data =
+                    NodeData::Leaf(LeafDataImpl::create(&index, leaf_modifications).await?);
                 let hash_value = TH::compute_node_hash(&node_data);
                 Self::write_to_output_map(output_map, index, hash_value, node_data)?;
                 Ok(hash_value)

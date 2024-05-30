861c375a49de4a1ab64917f6cc9ceb7ae8e0367b
Dori Medini <dori@starkware.co>
2024-05-30 17:04:49 +0300

    feat: added OriginalSkeletonInputNode enum to remove generic leaf from skeleton
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/committer/src/patricia_merkle_tree/original_skeleton_tree/node.rs
+++ crates/committer/src/patricia_merkle_tree/original_skeleton_tree/node.rs
@@ -1,5 +1,5 @@
 use crate::hash::hash_trait::HashOutput;
-use crate::patricia_merkle_tree::node_data::inner_node::PathToBottom;
+use crate::patricia_merkle_tree::node_data::inner_node::{BinaryData, EdgeData, PathToBottom};
 
 #[allow(dead_code)]
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
@@ -11,4 +11,11 @@ pub(crate) enum OriginalSkeletonNode {
     LeafOrBinarySibling(HashOutput),
     // Unmodified edge siblings bottom nodes on the merkle paths of modified leaves.
     UnmodifiedBottom(HashOutput),
+}
+
+/// A representation of the data required to build an original skeleton node.
+pub(crate) enum OriginalSkeletonInputNode {
+    Binary { hash: HashOutput, data: BinaryData },
+    Edge(EdgeData),
+    Leaf(HashOutput),
 }

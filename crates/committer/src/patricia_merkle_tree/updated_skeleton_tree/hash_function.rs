c8e757ba67b3a64d15cb3d212e9d444f4042d2ee
Dori Medini <dori@starkware.co>
2024-06-02 16:52:46 +0300

    chore: split TreeHashFunction hasher into leaf and non-leaf logic
    Signed-off-by: Dori Medini <dori@starkware.co>

--- crates/committer/src/patricia_merkle_tree/updated_skeleton_tree/hash_function.rs
+++ crates/committer/src/patricia_merkle_tree/updated_skeleton_tree/hash_function.rs
@@ -12,8 +12,30 @@ use crate::patricia_merkle_tree::node_data::leaf::{LeafData, LeafDataImpl};
 pub mod hash_function_test;
 
 pub(crate) trait TreeHashFunction<L: LeafData> {
+    /// Computes the hash of the given leaf.
+    fn compute_leaf_hash(leaf_data: &L) -> HashOutput;
+
     /// Computes the hash of given node data.
-    fn compute_node_hash(node_data: &NodeData<L>) -> HashOutput;
+    /// The default implementation for internal nodes is based on the following reference:
+    /// https://docs.starknet.io/documentation/architecture_and_concepts/Network_Architecture/starknet-state/#trie_construction
+    fn compute_node_hash(node_data: &NodeData<L>) -> HashOutput {
+        match node_data {
+            NodeData::Binary(BinaryData {
+                left_hash,
+                right_hash,
+            }) => HashOutput(Pedersen::hash(&left_hash.0.into(), &right_hash.0.into()).into()),
+            NodeData::Edge(EdgeData {
+                bottom_hash: hash_output,
+                path_to_bottom: PathToBottom { path, length },
+            }) => HashOutput(
+                Felt::from(Pedersen::hash(
+                    &hash_output.0.into(),
+                    &Felt::from(path).into(),
+                )) + length.0.into(),
+            ),
+            NodeData::Leaf(leaf_data) => Self::compute_leaf_hash(leaf_data),
+        }
+    }
 }
 
 pub struct TreeHashFunctionImpl;
@@ -30,46 +52,37 @@ impl TreeHashFunctionImpl {
 /// Implementation of TreeHashFunction. The implementation is based on the following reference:
 /// https://docs.starknet.io/documentation/architecture_and_concepts/Network_Architecture/starknet-state/#trie_construction
 impl TreeHashFunction<LeafDataImpl> for TreeHashFunctionImpl {
-    fn compute_node_hash(node_data: &NodeData<LeafDataImpl>) -> HashOutput {
-        HashOutput(match node_data {
-            NodeData::Binary(BinaryData {
-                left_hash,
-                right_hash,
-            }) => Pedersen::hash(&left_hash.0.into(), &right_hash.0.into()).into(),
-            NodeData::Edge(EdgeData {
-                bottom_hash: hash_output,
-                path_to_bottom: PathToBottom { path, length },
-            }) => {
-                Felt::from(Pedersen::hash(
-                    &hash_output.0.into(),
-                    &Felt::from(path).into(),
-                )) + length.0.into()
-            }
-            NodeData::Leaf(LeafDataImpl::StorageValue(storage_value)) => *storage_value,
-            NodeData::Leaf(LeafDataImpl::CompiledClassHash(compiled_class_hash)) => {
+    fn compute_leaf_hash(leaf_data: &LeafDataImpl) -> HashOutput {
+        match leaf_data {
+            LeafDataImpl::StorageValue(storage_value) => HashOutput(*storage_value),
+            LeafDataImpl::CompiledClassHash(compiled_class_hash) => {
                 let contract_class_leaf_version: Felt = Felt::from_hex(
                     Self::CONTRACT_CLASS_LEAF_V0,
                 )
                 .expect(
                     "could not parse hex string corresponding to b'CONTRACT_CLASS_LEAF_V0' to Felt",
                 );
-                Poseidon::hash(
-                    &contract_class_leaf_version.into(),
-                    &compiled_class_hash.0.into(),
+                HashOutput(
+                    Poseidon::hash(
+                        &contract_class_leaf_version.into(),
+                        &compiled_class_hash.0.into(),
+                    )
+                    .into(),
                 )
-                .into()
             }
-            NodeData::Leaf(LeafDataImpl::ContractState(contract_state)) => Pedersen::hash(
-                &Pedersen::hash(
+            LeafDataImpl::ContractState(contract_state) => HashOutput(
+                Pedersen::hash(
                     &Pedersen::hash(
-                        &contract_state.class_hash.0.into(),
-                        &contract_state.storage_root_hash.0.into(),
+                        &Pedersen::hash(
+                            &contract_state.class_hash.0.into(),
+                            &contract_state.storage_root_hash.0.into(),
+                        ),
+                        &contract_state.nonce.0.into(),
                     ),
-                    &contract_state.nonce.0.into(),
-                ),
-                &Self::CONTRACT_STATE_HASH_VERSION.into(),
-            )
-            .into(),
-        })
+                    &Self::CONTRACT_STATE_HASH_VERSION.into(),
+                )
+                .into(),
+            ),
+        }
     }
 }

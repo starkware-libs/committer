70fc408615389b29789469053f0011acdbb8929b
Nimrod Weiss <nimrod@starkware.co>
2024-06-03 12:52:46 +0300

    fix: fetch nodes didn't support empty tree

--- crates/committer/src/hash/hash_trait.rs
+++ crates/committer/src/hash/hash_trait.rs
@@ -6,4 +6,5 @@ pub struct HashOutput(pub Felt);
 impl HashOutput {
     #[allow(dead_code)]
     pub(crate) const ZERO: HashOutput = HashOutput(Felt::ZERO);
+    pub(crate) const EMPTY_NODE: HashOutput = Self::ZERO;
 }

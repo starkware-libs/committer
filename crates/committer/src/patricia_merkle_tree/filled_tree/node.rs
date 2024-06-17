8b883682d581a6d1570a10514548623aeb50006f
Nimrod Weiss <nimrod@starkware.co>
2024-06-17 11:03:09 +0300

    build: make filled node deserializable

--- crates/committer/src/patricia_merkle_tree/filled_tree/node.rs
+++ crates/committer/src/patricia_merkle_tree/filled_tree/node.rs
@@ -1,3 +1,5 @@
+use starknet_types_core::felt::FromStrError;
+
 use crate::felt::Felt;
 use crate::hash::hash_trait::HashOutput;
 use crate::patricia_merkle_tree::node_data::inner_node::NodeData;
@@ -8,11 +10,29 @@ use crate::patricia_merkle_tree::node_data::leaf::LeafData;
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct ClassHash(pub Felt);
 
+impl ClassHash {
+    pub(crate) fn from_hex(hex_string: &str) -> Result<Self, FromStrError> {
+        Ok(Self(Felt::from_hex(hex_string)?))
+    }
+}
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Nonce(pub Felt);
 
+impl Nonce {
+    pub(crate) fn from_hex(hex_string: &str) -> Result<Self, FromStrError> {
+        Ok(Self(Felt::from_hex(hex_string)?))
+    }
+}
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct CompiledClassHash(pub Felt);
+
+impl CompiledClassHash {
+    pub(crate) fn from_hex(hex_string: &str) -> Result<Self, FromStrError> {
+        Ok(Self(Felt::from_hex(hex_string)?))
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 /// A node in a Patricia-Merkle tree which was modified during an update.

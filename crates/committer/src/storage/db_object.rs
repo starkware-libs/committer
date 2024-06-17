e06aecd13710173b91fe4c4ba5d3ec830fd00dbd
Nimrod Weiss <nimrod@starkware.co>
2024-06-17 11:03:09 +0300

    build: make filled node deserializable

--- crates/committer/src/storage/db_object.rs
+++ crates/committer/src/storage/db_object.rs
@@ -21,9 +21,11 @@ pub trait DBObject {
     }
 }
 
-pub(crate) trait Deserializable: Sized {
+pub trait Deserializable: Sized {
     /// Deserializes the given value.
-    fn deserialize(key: &StorageKey, value: &StorageValue) -> Result<Self, DeserializationError>
-    where
-        Self: Sized;
+    fn deserialize(
+        key: &StorageKey,
+        value: &StorageValue,
+        prefix: &StoragePrefix,
+    ) -> Result<Self, DeserializationError>;
 }

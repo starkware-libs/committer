7734ba7a8c8b4e377a3f94c5f33d19f130bbb5de
Aner Ben Efraim <aner@starkware.co>
2024-06-20 11:05:04 +0300

    fix: ci for bench_test

--- crates/committer_cli/src/tests/benchmark_tests.rs
+++ crates/committer_cli/src/tests/benchmark_tests.rs
@@ -6,7 +6,9 @@ use serde_json::Value;
 
 use crate::tests::utils::parse_from_python::parse_input_single_storage_tree_flow_test;
 
-//TODO(Aner, 17/06/2024): should the MAX_TIME_FOR_BECHMARK_TEST be for --release or --debug?
+//TODO(Aner, 20/06/2024): this test needs to be fixed to be run correctly in the CI:
+//1. Fix the test to measure cpu_time and not wall_time.
+//2. Fix the max time threshold to be the expected time for the benchmark test.
 const MAX_TIME_FOR_BECHMARK_TEST: f64 = 5.0;
 const INPUT: &str = include_str!("../../inputs.json");
 

e9975c2ef587b0d56b5ff171edbc6a04fbf8298e
Aner Ben Efraim <aner@starkware.co>
2024-07-08 18:17:09 +0300

    refactor: using structs for deserialization

--- crates/committer_cli/src/tests/utils/parse_from_python.rs
+++ crates/committer_cli/src/tests/utils/parse_from_python.rs
@@ -12,19 +12,50 @@ use ethnum::U256;
 use std::collections::HashMap;
 
 #[allow(clippy::unwrap_used)]
+pub fn deserialize_input_single_storage_tree_flow_test(
+    input: String,
+) -> (
+    LeafModifications<StarknetStorageValue>,
+    MapStorage,
+    HashOutput,
+) {
+    parse_input_single_storage_tree_flow_test_from_map(serde_json::from_str(&input).unwrap())
+}
+#[allow(clippy::unwrap_used)]
+pub fn parse_input_single_storage_tree_flow_test_from_map(
+    // TODO(Aner, 8/7/2024): use struct for deserialization.
+    input: HashMap<String, String>,
+) -> (
+    LeafModifications<StarknetStorageValue>,
+    MapStorage,
+    HashOutput,
+) {
+    // TODO(Aner, 8/7/2024): use struct for deserialization.
+    let leaf_modifications_json = input.get("leaf_modifications").unwrap();
+    let storage_json = input.get("storage").unwrap();
+    let root_hash = input.get("root_hash").unwrap();
+
+    parse_input_single_storage_tree_flow_test(
+        leaf_modifications_json.to_string(),
+        storage_json.to_string(),
+        root_hash.to_string(),
+    )
+}
+#[allow(clippy::unwrap_used)]
 /// Parse input for single storage tree flow test.
 /// Returns the leaf modifications, fetched nodes (in storage) and the root hash.
 pub fn parse_input_single_storage_tree_flow_test(
-    input: &HashMap<String, String>,
+    leaf_modifications_json: String,
+    storage_json: String,
+    root_hash: String,
 ) -> (
     LeafModifications<StarknetStorageValue>,
     MapStorage,
     HashOutput,
 ) {
     // Fetch leaf_modifications.
-    let leaf_modifications_json = input.get("leaf_modifications").unwrap();
     let leaf_modifications_map =
-        serde_json::from_str::<HashMap<&str, &str>>(leaf_modifications_json).unwrap();
+        serde_json::from_str::<HashMap<&str, &str>>(&leaf_modifications_json).unwrap();
     let leaf_modifications = leaf_modifications_map
         .iter()
         .map(|(k, v)| {
@@ -36,8 +67,7 @@ pub fn parse_input_single_storage_tree_flow_test(
         .collect();
 
     // Fetch storage.
-    let raw_storage =
-        serde_json::from_str::<Vec<RawStorageEntry>>(input.get("storage").unwrap()).unwrap();
+    let raw_storage = serde_json::from_str::<Vec<RawStorageEntry>>(&storage_json).unwrap();
 
     let mut storage = HashMap::new();
     for entry in raw_storage {
@@ -53,7 +83,7 @@ pub fn parse_input_single_storage_tree_flow_test(
     let map_storage = MapStorage { storage };
 
     // Fetch root_hash.
-    let root_hash = HashOutput(Felt::from_hex(input.get("root_hash").unwrap()).unwrap());
+    let root_hash = HashOutput(Felt::from_hex(&root_hash).unwrap());
 
     (leaf_modifications, map_storage, root_hash)
 }

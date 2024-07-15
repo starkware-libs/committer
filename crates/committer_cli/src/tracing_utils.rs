a4edb7077bdb0d1572491e9a4a62ae1bcaee2244
amos rothberg <amos@starkware.co>
2024-07-15 11:26:50 +0300

    Update tracing_utils.rs

--- crates/committer_cli/src/tracing_utils.rs
+++ crates/committer_cli/src/tracing_utils.rs
@@ -1,25 +1,27 @@
 use tracing::metadata::LevelFilter;
+use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::{fmt, EnvFilter};
 
 const DEFAULT_LEVEL: LevelFilter = LevelFilter::INFO;
 
-// TODO(Amos, 1/8/2024): Actually understand the code in this file and assert it's what we need.
-// It is the same as Papyrus and mempool-dev, except I added `.with_ansi(false)`
-// TODO(Amos, 1/8/2024): Delete the below TODOs or move to my name.
-// TODO(yair): add dynamic level filtering.
-// TODO(dan): filter out logs from dependencies (happens when RUST_LOG=DEBUG)
-// TODO(yair): define and implement configurable filtering.
-pub fn configure_tracing() {
-    let fmt_layer = fmt::layer().compact().with_target(false).with_ansi(false);
-    let level_filter_layer = EnvFilter::builder()
-        .with_default_directive(DEFAULT_LEVEL.into())
-        .from_env_lossy();
+// // TODO(dan): filter out logs from dependencies (happens when RUST_LOG=DEBUG)
+// pub fn configure_tracing() {
+//     let fmt_layer = fmt::layer()
+//         .compact()
+//         .with_span_events(FmtSpan::CLOSE | FmtSpan::ENTER)
+//         .with_ansi(false);
+//     let level_filter_layer = EnvFilter::builder()
+//         .with_default_directive(DEFAULT_LEVEL.into())
+//         .from_env_lossy();
 
-    // This sets a single subscriber to all of the threads. We may want to implement different
-    // subscriber for some threads and use set_global_default instead of init.
-    tracing_subscriber::registry()
-        .with(fmt_layer)
-        .with(level_filter_layer)
-        .init();
+//     // This sets a single subscriber to all of the threads.
+//     tracing_subscriber::registry()
+//         .with(fmt_layer)
+//         .with(level_filter_layer)
+//         .init();
+// }
+
+pub fn configure_tracing() {
+    tracing_subscriber::fmt().init();
 }
